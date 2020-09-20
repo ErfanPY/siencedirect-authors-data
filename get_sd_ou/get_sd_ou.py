f1c0cc1e4c55205cede010f92e8880916a7f1e93
Erfan <parsyab1@gmail.com>
2020-09-20 22:52:16 +0430

    sort imports and add worker and main function

--- get_sd_ou/get_sd_ou.py
+++ get_sd_ou/get_sd_ou.py
@@ -1,15 +1,16 @@
 #%%
-from get_sd_ou.class_util import Page, Seen_table
-from get_sd_ou import database_util as dbutil
-
 import logging
 import queue
 import re
+from re import search
 import threading
 from urllib.parse import parse_qsl, unquote_plus, urljoin, urlparse
 
 import requests
 from bs4 import BeautifulSoup as bs
+
+from get_sd_ou.databse_util import get_article_authors
+from get_sd_ou.class_util import Article, Search_page
 
 
 fh = logging.FileHandler('logs.log')
@@ -26,20 +27,7 @@ headers = {
         }
 
 
-seen_table = Seen_table()
-
 main_queue = queue.Queue()
-
-
-def add_queue(base_url, url):
-    #join the url -> Page the url -> add to queue & add to seen_table
-    url = urljoin(base_url, url)
-    page = Page(url)
-    if seen_table.is_in(page):
-        return
-    seen_table.add_url(page)
-    main_queue.put(page)
-
 
 def soup_maker (url, headers={}):
     try:
@@ -49,46 +37,36 @@ def soup_maker (url, headers={}):
     soup = bs(content, 'html.parser')
     return soup
 
-###### SEARCH PAGE TEST
-base_url = 'https://www.sciencedirect.com/'
-search_url = 'https://www.sciencedirect.com/search?date={}&show={}&sortBy=date'
-article_history = {} 
-author_history = {}
-start_year = 2010
-end_year = 2020
+def main():
 
-year = '2020'
-show_count = 25
+    ###### SEARCH PAGE TEST
+    base_url = 'https://www.sciencedirect.com/'
+    search_url = 'https://www.sciencedirect.com/search?date={}&show={}&sortBy=date'
+    article_history = {} 
+    author_history = {}
+    start_year = 2010
+    end_year = 2020
 
-search_page = Page(search_url.format(year, show_count), do_soup=True, headers=headers)
+    year = '2020'
+    show_count = 25
 
-#%%
-search_result = search_page.soup.find_all('a')
-articles = []
-for article in search_result :
-    if article.get('href'):
-        article_link = article.get('href')
-        if 'pii' in article_link and not 'pdf' in article_link:
-            articles.append(urljoin(base_url, article_link))
-
-#%%
+#%%-
 def worker():
     while True:
-        page_inst = main_queue.get()
-        #get all links and add them to main_queue
-        #get all text and add to item.urls
-    
-            
-        urls = page_inst.get_urls()
-        for i_url in urls:
-            add_queue(i_url)
-                
+        if main_queue.empty():
+            next_search = Search_page(next(search_url_gen))
+            next_articles = next_search.get_articles()
+            [main_queue.put(next_article) for next_article in next_articles]
+
+        article_url = main_queue.get()
+        article = Article(article_url, headers)
+        article_data = article.get_data()
+        dbutil.insert_article_data(article_data)
+        
         main_queue.task_done()
     
 [threading.Thread(target=worker).start() for _ in range(2)]
 
 # block until all tasks are done
 main_queue.join()
-print('All work completed')    
-
-# %%
+print('All work completed')
