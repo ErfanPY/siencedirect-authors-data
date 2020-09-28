70b5043791e116bf4f19c838b08bc0500be59490
Erfan <parsyab1@gmail.com>
2020-09-28 22:10:53 +0330

    logger fileConfig -> dictConfig

--- get_sd_ou/__init__.py
+++ get_sd_ou/__init__.py
@@ -1,9 +1,45 @@
+from logging.config import dictConfig
 import queue
 import threading
 from urllib.parse import parse_qsl, unquote_plus, urljoin, urlparse
 
 import requests
 from bs4 import BeautifulSoup as bs
-import logging
+import logging.config
 
-logging.config.fileConfig('logging.conf')
+LOGGING_CONFIG = { 
+    'version': 1,
+    'disable_existing_loggers': True,
+    'formatters': { 
+        'standard': { 
+            'format': '%(asctime)s [%(levelname)s] %(name)s: %(message)s'
+        },
+    },
+    'handlers': { 
+        'default': { 
+            'level': 'INFO',
+            'formatter': 'standard',
+            'class': 'logging.StreamHandler',
+            'stream': 'ext://sys.stdout',  # Default is stderr
+        },
+    },
+    'loggers': { 
+        '': {  # root logger
+            'handlers': ['default'],
+            'level': 'WARNING',
+            'propagate': False
+        },
+        'mainLogger': { 
+            'handlers': ['default'],
+            'level': 'DEBUG',
+            'propagate': False
+        },
+        '__main__': {  # if __name__ == '__main__'
+            'handlers': ['default'],
+            'level': 'DEBUG',
+            'propagate': False
+        },
+    } 
+}
+
+logging.config.dictConfig(LOGGING_CONFIG)
