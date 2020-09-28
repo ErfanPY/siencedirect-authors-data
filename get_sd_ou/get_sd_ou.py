5765eb9ba9949e812acca353dbf33b59481cf187
Erfan <parsyab1@gmail.com>
2020-09-28 19:07:04 +0330

    logger, main search

--- get_sd_ou/get_sd_ou.py
+++ get_sd_ou/get_sd_ou.py
@@ -1,25 +1,16 @@
 #%%
 import queue
+from re import search
 import threading
 from urllib.parse import parse_qsl, unquote_plus, urljoin, urlparse
 
 import requests
 from bs4 import BeautifulSoup as bs
 
-from get_sd_ou.databse_util import get_article_authors
-from get_sd_ou.class_util import Article, Search_page
-from get_sd_ou import init_logger
-
-logger = init_logger()
-
-headers = {
-        'Accept' : 'application/json',
-        'user-agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:80.0) Gecko/20100101 Firefox/80.0'
-        }
-
-
-main_queue = queue.Queue()
-
+from .__init__ import *
+from .class_util import Article, Search_page
+from .database_util import *
+#%%
 def soup_maker (url, headers={}):
     try:
         content = requests.get(url, headers=headers).content
@@ -28,33 +19,62 @@ def soup_maker (url, headers={}):
     soup = bs(content, 'html.parser')
     return soup
 
-def search_gen(year, show_per_page):
-    offset = 0
-    search_url = f'https://www.sciencedirect.com/search?date={year}&show={show_per_page}&offset={offset}&sortBy=date'
-    pages_count = Search_page(search_url).pages_count
-    total_page_count = show_per_page * pages_count
-    while(offset < total_page_count):
+def next_year_gen(init_year=2020, year_step=-1):
+    """ iterate through all year fron init_year """
+    logger.debug('[main] [next_year_gen] initiated')
+    current_search_year = init_year
+    while(True):
+        yield current_search_year
+        logger.debug('[main] [next_year_gen] next made')
+        current_search_year += year_step
+
+def next_page_gen(year, show_per_page=100):
+    """ iterate through page of year """
+    logger.debug('[main] [next_page_gen] initiated')
+    search_url = f'https://www.sciencedirect.com/search?date={year}&show={show_per_page}&sortBy=date'
+    while True :
         yield search_url
-        offset += show_per_page
+        logger.debug('[main] [next_page_gen] next made')
+        search = Search_page(search_url)
+        search_url = search.next_page()
+
 
 #%%-
 def worker():
-    while True:
+    global next_page_gen_obj
+    continue_search = True
+    while continue_search:
         if main_queue.empty():
-            logger.debug('get next search page')
-            next_search = Search_page(next(search_url_gen_obj))
-            next_articles = next_search.get_articles()
-            [main_queue.put(next_article) for next_article in next_articles]
-
+            next_page = next(next_page_gen_obj)
+            if next_page:
+                logger.debug('[worker] get page')
+                search = Search_page(next_page)
+                articles = search.get_articles()
+                [main_queue.put(article) for article in articles]
+                logger.debug('[worker] next page article added')
+            else:
+                logger.debug('[worker] get next year')
+                next_year = next(next_year_gen_obj)
+                next_page_gen_obj = next_page_gen(next_year)
+                logger.debug('[worker] current year set to next year')
+                continue
         logger.debug('get articles from serach')
         article_url = main_queue.get()
         article = Article(article_url, headers)
-        article_data = article.get_data()
-        dbutil.insert_article_data(article_data)
+        article_data = article.get_article_data()
+        insert_article_data(article_data)
         
         main_queue.task_done()
 
+#%%
+
+headers = {
+        'Accept' : 'application/json',
+        'user-agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:80.0) Gecko/20100101 Firefox/80.0'
+        }
+
 base_url = 'https://www.sciencedirect.com/'
+article_test_url = 'https://www.sciencedirect.com/science/article/pii/S0950423020305763#!'
 article_history = {} 
 author_history = {}
 start_year = 2010
@@ -63,7 +83,43 @@ end_year = 2020
 year = '2020'
 show_per_page = 25
 
+main_queue = None
+threads = None
+next_year_gen_obj = None
+next_page_gen_obj = None
+#%%
+def start_search(init_year):
+    global threads
+    global main_queue
+    global next_year_gen_obj
+    global next_page_gen_obj
+    """ 
+    1) Initiate the threads
+    2) Initiate the database connection
+    3) Call the worker function of each thread
+    """
+    logger.info('[main] search started')
+    next_year_gen_obj = next_year_gen(init_year=init_year)
+    next_page_gen_obj = next_page_gen(next(next_year_gen_obj), show_per_page=show_per_page)
+    main_queue = queue.Queue()
+    #threads = [threading.Thread(target=worker) for _ in range(2)]
+    #[thread.start() for thread in threads]
+    #logger.debug('[main] threads started')
+    worker()
+    main_queue.join()
+    return {'status':'200', 'msg':'threads started succesfully', 'threads':threads}
 
-[threading.Thread(target=worker).start() for _ in range(2)]
+def pause_search():
+    logger.info('[main] search paused')
+    pass
+def stop_search():
+    """ 
+    1) Commit current state to database
+    2) kill threads
+    """
+    logger.info('[main] search stoped')
+    pass
 
-main_queue.join()
+start_search(2020)
+
+print('Fuck this life')
