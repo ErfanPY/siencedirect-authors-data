dedd87a7fdc9e0eff3934b5d08f2219c0b590acb
Erfan <parsyab1@gmail.com>
2020-09-29 22:52:55 +0330

    make adatabse manage object oriented

--- get_sd_ou/database_util.py
+++ get_sd_ou/database_util.py
@@ -1,27 +1,80 @@
 import mysql.connector
-from .__init__ import *
+try:
+  from .__init__ import *
+except ImportError:
+  from get_sd_ou.__init__ import *
 logger = logging.getLogger('mainLogger')
 
-def init_database():
-  mydb = mysql.connector.connect(
-    host="localhost",
-    user="root",
-    password="123456",
-    database="siencedirect"
-  )
-  return mydb
+_database = None
 
-def add_article(database, pii, title=None):
+class Database():
+  def __init__(self, host,  user, passwrord, dbname, **kwargs):
+    self.database = mysql.connector.connect(
+      host=host,
+      user=user,
+      password=passwrord,
+      database=dbname
+    )
+
+
+class Table(Database):
+  def __init__(self, table_name, **kwargs):
+    super().__init__(host)
+    self.table_name = table_name
+  def insert():
+    pass
+  def selecet():
+    pass
+  def is_row_exist(self, **kwargs):
+    column = kwargs.keys()[0]
+    val = kwargs[column]
+    sql = "SELECT EXISTS(SELECT 1 FROM %s WHERE %s='%s' LIMIT 1)"
+    val = (self.table_name, column, value)
+    database.cursor.execute(sql, val)
+    result = database.cursor.fetch()
+    return result
+
+
+class ArticleTable(Table):
+  def __init__(self):
+    super().__init__(table_name='articles')
+  
+  def article(self, pii):
+    if not self.is_row_exist(pii=pii):
+      self.insert(pii=pii)
+    return self.select(pii=pii)
+  
+  def is_row_exist(self, **kwargs):
+    return super().is_row_exist()
+
+class AuthorTable(Table):
+  def __init__(self, name):
+    super().__init__(table_name='authors')
+
+
+def init_database():
+  global _database
+  if not _database:
+    _database = mysql.connector.connect(
+      host="localhost",
+      user="root",
+      password="123456",
+      database="siencedirect"
+    )
+  return _database
+
+### INSERT
+
+def insert_article(database, pii, title=None):
+  database  = database if database else init_database()
   #update = UPDATE articles SET title=%S
   sql = "INSERT INTO articles (pii, title) VALUES (%s, %s);"
   val = (pii, title)
   database.cursor.execute(sql, val)
   database.commit()
 
-def update_article():
-  raise NotImplementedError
-
-def add_author(database, name, email='', id='', mendely='', scopus='', affiliation=''):
+def insert_author(database, name, email='', id='', mendely='', scopus='', affiliation=''):
+  database  = database if database else init_database()
   raise NotImplementedError
   """
   sql = "INSERT INTO authors (name) VALUES (%s);"
@@ -29,20 +82,53 @@ def add_author(database, name, email='', id='', mendely='', scopus='', affiliati
   database.cursor.execute(sql, val)
   database.commit()
   """
-
-def update_author():
-  raise NotImplementedError
-
 def connect_article_authors(database, article_id, author_id, is_coresponde=False):
+  database  = database if database else init_database()
   #TODO connect article with pii (get article id from articles from pii)
   sql = "INSERT INTO article_authors (article_id, author_id) VALUES (%s, %s);"
   val = (article_id, author_id)
   database.cursor.execute(sql, val)
 
-def insert_article_data(database):
-  pass
+def insert_multi_author(database, authors):
+  database  = database if database else init_database()
+  #multi insert
+  sql = "INSERT INTO authors (name, email) VALUES (%s, %s)"
+  val = [(author.name, '') for author in authors]
+
+  database.cursor.executemany(sql, val)
+
+def insert_article_data(database, pii, authors):
+  database  = database if database else init_database()
+  article_id = insert_article(pii=pii)
+  
+  for author in authors:
+    name = author.name
+    author_id = insert_author(database, name)
+    connect_article_authors(article_id, author_id, author.is_coresponde)
+  sql = ""
+  database
+
+### UPDATE
+
+def update_article():
+  raise NotImplementedError
+
+def update_author(database,):
+  database  = database if database else init_database()
+  raise NotImplementedError
+
+### SELECT
+
+def is_row_exist(database, table, column, value):
+  database  = database if database else init_database()
+  sql = "SELECT EXISTS(SELECT 1 FROM %s WHERE %s='%s' LIMIT 1)"
+  val = (table, column, value)
+  database.cursor.execute(sql, val)
+  result = database.cursor.fetch()
+  return result
 
 def get_article_authors(database, article_id):
+  database  = database if database else init_database()
   sql = "SELECT t1.title, t3.name\
           FROM articles AS t1\
           JOIN article_authors AS t2 ON t1.article_id = t2.article_id\
@@ -55,33 +141,6 @@ def get_article_authors(database, article_id):
   myresult = database.cursor.fetchall()
   return myresult
 
-def add_article_data(database, article_data):
-  article_id = add_article(*article_data['article'])
-  for author in article_data['authors']:
-    author_id = add_author(*author)
-    connect_article_authors(article_id, author_id, author.is_coresponde)
-
 def get_articles_of_author(database, sql, val):
+  database  = database if database else init_database()
   database.cursor.execute(sql, val)
-
-
-def multi_inser(database):
-  #multi insert
-  sql = "INSERT INTO customers (name, address) VALUES (%s, %s)"
-  val = [
-    ('Peter', 'Lowstreet 4'),
-    ('Amy', 'Apple st 652'),
-    ('Hannah', 'Mountain 21'),
-    ('Michael', 'Valley 345'),
-    ('Sandy', 'Ocean blvd 2'),
-    ('Betty', 'Green Grass 1'),
-    ('Richard', 'Sky st 331'),
-    ('Susan', 'One way 98'),
-    ('Vicky', 'Yellow Garden 2'),
-    ('Ben', 'Park Lane 38'),
-    ('William', 'Central st 954'),
-    ('Chuck', 'Main Road 989'),
-    ('Viola', 'Sideway 1633')
-  ]
-
-  database.cursor.executemany(sql, val)
