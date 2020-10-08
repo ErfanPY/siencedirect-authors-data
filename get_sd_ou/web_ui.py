743e1702e5ec5c2fdd380b842166e048fd382a58
ErfanPy <parsyab1@gmail.com>
2020-10-08 23:06:21 +0330

    add base flask components

--- get_sd_ou/web_ui.py
+++ get_sd_ou/web_ui.py
@@ -1,17 +1,31 @@
-from flask import Flask, Response
-from .__init__ import *
-from get_sd_ou.get_sd_ou import *
+from flask import Flask, Response, redirect, render_template
+
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
+
+import threading
+from get_sd_ou.get_sd_ou import start_search
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'you-will-never-guess'
 
 search_thread = None
+is_search_running = False
+
+class StartForm(FlaskForm):
+   start_year = StringField('Start year', validators=[DataRequired()])
+   affiliation = StringField('Affiliation')
+   submit = SubmitField('Start')
 
 @app.route('/start')
 def start ():
+   form = StartForm()
+   return render_template('start.html', form=form)
    global search_thread
    search_thread = threading.Thread(target=start_search, args=(2018,))
    search_thread.start()
-   return 'Started'
+   
 
 @app.route('/stop')
 def stop ():
@@ -20,9 +34,16 @@ def stop ():
 
 @app.route('/status')
 def status ():
-   with open('get_sd_ou/get_sd_ou.log', 'r') as f:
-      content = f.readlines()[::-1]
-   return Response(content, mimetype='text/plain')
+   if is_search_running:
+      with open('get_sd_ou/get_sd_ou.log', 'r') as f:
+         content = f.readlines()[::-1]
+      return Response(content, mimetype='text/plain')
+   return redirect('/start')
+
+@app.route('/dbstat')
+def dbstat ():
+   global search_thread
+   del search_thread
 
 @app.route('/article/<pii>')
 def article_data(pii):
@@ -32,4 +53,4 @@ def article_data(pii):
 def author_data(name):
    pass
 
-app.run(host='0.0.0.0')
+app.run(host='0.0.0.0', debug=True)
