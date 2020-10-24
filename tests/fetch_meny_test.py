422926a0e11df9f7c0b8d84998ac889934f365dc
ErfanPy <parsyab1@gmail.com>
2020-10-24 22:47:04 +0330

    remove  tests (not unit test or function tests)

--- tests/fetch_meny_test.py
+++
@@ -1,5 +0,0 @@
-import mysql.connector as co
-cnx = co.connect(host='localhost', user='sciencedirect', password='root', port='3306', database='sciencedirect')
-cursor = cnx.cursor(buffered=True)
-cursor.execute('SELECT name FROM sciencedirect.authors WHERE scopus is NULL')
-print(cursor.fetchmany(10))
