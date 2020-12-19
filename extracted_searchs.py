01b19ea642afd51addc7e39138eb51a01e0dcf32
Erfan <parsyab1@gmail.com>
2020-12-19 15:47:33 +0330

    queue based coroutine

--- extracted_searchs.py
+++ extracted_searchs.py
@@ -15,7 +15,7 @@ from get_sd_ou.database_util import (connect_search_article, init_db,
 
 async def get_soup(session, url):
     async with session.get(url) as response:
-        return  await response.read()
+        return  bs(await response.read(), 'html.parser')
         
 async def parse_article(session, article_url, search_url, db_cnx):
     article_content = await get_soup(session, article_url)
@@ -31,36 +31,19 @@ async def parse_article(session, article_url, search_url, db_cnx):
 
     connect_search_article(search_id=search_id, article_id=article_id, cnx=db_cnx)
 
-async def parse_search(session, search_url, search_name):
-    search_content = await get_soup(session, search_url)
-    search_soup = bs(search_content, 'html.parser')
+async def parse_search(session, queue):
+    while True:
+        search_name, search_url = await queue.get()
+        print(search_name, search_url)
+        search_soup = await get_soup(session, search_url)
     search_page = Search_page(url=search_url, soup_data=search_soup)
     
     with open(os.path.join('./extracted_articles', search_name), 'a') as file:
         file.write(search_url+'\n')
         file.writelines([i+'\n' for i in search_page.get_articles()])
+        print('3 :: ', search_name, search_url)
+        queue.task_done()
 
-
-async def start_searchs_parse(search_items):
-    async with aiohttp.ClientSession() as session:
-        tasks = []
-        for search_name, search_url in search_items:
-            task = asyncio.ensure_future(parse_search(session, search_url, search_name))
-            tasks.append(task)
-        await asyncio.gather(*tasks, return_exceptions=True)
-
-def start_start_serach(free_or_limited_search='a'):
-    file_list = [os.path.join('./search_files', file) for file in os.listdir('./search_files')]
-    search_items = []
-    for file_path in file_list:
-        search_name = file_path.split('\\')[-1].split('.')[0]
-        with open(file_path) as file:
-            lines = file.readlines()
-            for line in lines :
-                line = line.strip() if line and not line == ' ' and not line == '\n' else ''
-                search_items.append((search_name, line))
-
-    start_time = time.time()
     def filter_search(search_url, free_or_limited_search):
         offset = int(dict(parse_qsl(urlparse(search_url).query)).get('offset', 0))
         if free_or_limited_search == 'f':
@@ -70,52 +53,61 @@ def start_start_serach(free_or_limited_search='a'):
         else :
             return 1
     
-    search_items = list(filter(lambda item: filter_search(item[1], free_or_limited_search), search_items))
+def get_searchs_from_dir(dir_path):
+    file_list = [os.path.join(dir_path, file) for file in os.listdir(dir_path)]
+    search_items = []
+    for file_path in file_list:
+        search_name = file_path.split('\\')[-1].split('.')[0]
         
-    for i in range(0, len(search_items), async_slice_size):
-        search_slice_items = search_items[i:i+async_slice_size]
+        with open(file_path) as file:
+            search_items = [(search_name, line.strip()) for line in file.readlines() if line.strip()]
+    # search_items = list(filter(lambda item: filter_search(item[1], free_or_limited_search), search_items))
 
-        slice_start_time = time.time()
+    return search_items
 
-        asyncio.run(start_searchs_parse(search_items=search_slice_items))
 
-        slice_end_time = time.time()
-        slice_duration = slice_end_time - slice_start_time
+async def start_searchs_parse(free_or_limited_search='a'):
+    search_urls = get_searchs_from_dir('./search_files')
+    queue = asyncio.Queue()
+    workers_count = 3
 
-        print(f'Prosseced {async_slice_size} in {slice_duration} second')
-        with open('result_time.txt', 'a') as f:
-            f.write(f'Prosseced {async_slice_size} in {slice_duration} second')
+    for search_name, search_url in search_urls:
+            queue.put_nowait((search_name, search_url))
     
-    duration = time.time()-start_time
-    with open('result_time.txt', 'a') as f:
-            f.write(f'{len(search_items)} search link in {duration}s')
-    input(f'{len(search_items)} search link in {duration}s')
-
-async def start_articles_parse(search_article_items):
+    tasks = []
     async with aiohttp.ClientSession() as session:
-        tasks = []
-        db_cnx = init_db()
-        for search_url, article_url in search_article_items:
-            task = asyncio.ensure_future(parse_article(session=session, article_url=article_url, search_url=search_url, db_cnx=db_cnx))
+        for _ in range(workers_count):
+            task = asyncio.ensure_future(parse_search(session, queue))
             tasks.append(task)
+
+        started_at = time.monotonic()
+        await queue.join()
+    total_slept_for = time.monotonic() - started_at
+
+    for task in tasks:
+        task.cancel()
+
         await asyncio.gather(*tasks, return_exceptions=True)
 
-def start_start_article():
-    file_list = [os.path.join('./extracted_articles', file) for file in os.listdir('./extracted_articles')]
-    search_article_items = []
+    print('====')
+    print(f'{workers_count} workers Got {len(search_urls)} search in {total_slept_for:.2f} seconds')
 
+def get_articles_from_dir(dir_path):
+    file_names = os.listdir(dir_path)
+    file_list = [os.path.join(dir_path, file) for file in file_names]
+    
+    search_article_items = []
     for file_path in file_list:
         with open(file_path) as file:
-            lines = file.readlines()
+            lines = [line.strip() for line in file.readlines() if line.strip()]
             current_search = ''
-            for line in lines :
-                line = line.strip() if line and not line == ' ' and not line == '\n' else ''
+            for line in lines:
                 if 'search' in line:
                     current_search = line
                 elif 'article' in line:
                     search_article_items.append((current_search, line))
 
-    start_time = time.time()
+    return search_article_items
     
     for i in range(0, len(search_article_items), async_slice_size):
         article_slice_items = search_article_items[i:i+async_slice_size]
@@ -149,22 +141,16 @@ def test_extraction():
 
 if __name__ == '__main__':
     logger = logging.getLogger('mainLogger')
-    # logger.disabled = True
+    debug = False
+    logger.disabled = not debug
 
     async_slice_size = 700
-    article_or_search = 'a' # a for article, s for search :)
-    free_or_limited_search = 'f' # f for free (just search accesible for every one), l for limited (just those accesible for registered), a for all
+    search_mode = 's' # a: article, s: search, t: testing extracted articles :)
+    free_or_limited_search = 'f' # f: free (just search accesible for every one), l: limited (just those accesible for registered), a: all
     
-    if article_or_search == 'a':
-        start_start_article()
-    elif article_or_search == 's' :
-        start_start_serach(free_or_limited_search)
-
-# async def download_all_sites(sites):
-#     async with aiohttp.ClientSession() as session:
-#         tasks = []
-#         for i in range(0, len(sites), 1000):    
-#             for url in sites[i:i+1000]:
-#                 task = asyncio.ensure_future(download_site(session, url))
-#                 tasks.append(task)
-#         await asyncio.gather(*tasks, return_exceptions=True)
+    if search_mode == 'a':
+        asyncio.run(start_articles_parse(), debug=debug)
+    elif search_mode == 's' :
+        asyncio.run(start_searchs_parse(free_or_limited_search), debug=debug)
+    elif search_mode == 't':
+        test_extraction()
