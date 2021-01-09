d0423d27cb3e5037cd1f22ac30f3a2da33997846
Erfan <parsyab1@gmail.com>
2021-01-09 13:07:41 +0330

    format and delete extra codes

--- extracted_searchs.py
+++ extracted_searchs.py
@@ -5,7 +5,6 @@ import os
 from urllib.parse import parse_qsl, urlparse
 
 import aiohttp
-# from aiohttp.helpers import current_task
 from bs4 import BeautifulSoup as bs
 
 from get_sd_ou.class_util import Article, Search_page
@@ -21,30 +20,6 @@ async def get_soup(session, url):
             return e
 
 
-async def parse_search(session, search_url, search_name):
-    search_content = await get_soup(session, search_url)
-    if not search_content:
-        return search_url, []
-    search_soup = bs(search_content, 'html.parser')
-    search_page = Search_page(url=search_url, soup_data=search_soup)
-
-    articles = search_page.get_articles()
-    logger.log(10001, search_url + ' || ' + str(len(articles)))
-    ext_path = os.path.join('./extracted_articles', search_name+".json")
-    with open(ext_path, 'r+') as file:
-        try:
-            search_dict = json.load(file)
-        except json.decoder.JSONDecodeError:
-            search_dict = {}
-
-        search_dict[search_url] = list(
-            set(search_dict.get(search_url, []) + articles))
-        file.seek(0)
-        json.dump(search_dict, file)
-        file.truncate()
-    return search_name, search_url, articles
-
-
 def filter_search(search_url, free_or_limited_search):
     offset = int(dict(parse_qsl(urlparse(search_url).query)).get('offset', 0))
     if free_or_limited_search == 'f':
@@ -53,30 +28,6 @@ def filter_search(search_url, free_or_limited_search):
         return offset >= 1000
     else:
         return 1
-
-
-async def start_searchs_parse(search_items):
-    async with aiohttp.ClientSession() as session:
-        tasks = []
-        for search_name, search_urls in search_items:
-            for search_url in search_urls:
-                task = asyncio.ensure_future(
-                    parse_search(session, search_url, search_name))
-                tasks.append(task)
-            search_result = await asyncio.gather(*tasks, return_exceptions=True)
-            # search_groups = [list(item[1]) for item in itertools.groupby(sorted(search_result), key=lambda x: x[0])]
-            # for search_group in search_groups:
-            #     search_name = search_group[0][0]
-            #     with open(os.path.join('./extracted_articles', search_name+".json"), 'w+') as file:
-            #         try :
-            #             search_dict = json.load(file)
-            #         except json.decoder.JSONDecodeError:
-            #             search_dict = {}
-            #         for _, search_url, articles in search_group:
-
-            #             search_dict[search_url] = list(set(search_dict.get(search_url, []) + articles))
-            #             json.dump(search_dict, file)
-            #             logger.log(10001, f'{"END": >5} || {search_name} : {len(articles)} || {search_url}')
 
 
 def is_in_extracted(search_name, url):
@@ -103,6 +54,7 @@ def get_search_from_dir(dir, free_or_limited_search):
             search_dict[search_name] = search_urls
     return list(search_dict.items())
 
+
 def log_formatter(msg, data):
     ret = ''
     ret += f'[{msg}]'
@@ -110,9 +62,83 @@ def log_formatter(msg, data):
         ret += f' {key}: {value} |'
     return ret[:-2]
 
+
+async def start_searchs_parse(search_items):
+    async with aiohttp.ClientSession() as session:
+        tasks = []
+        for search_name, search_urls in search_items:
+            for search_url in search_urls:
+                task = asyncio.ensure_future(
+                    parse_search(session, search_url, search_name))
+                tasks.append(task)
+            search_result = await asyncio.gather(*tasks, return_exceptions=True)
+
+
+async def parse_search(session, search_url, search_name):
+    search_content = await get_soup(session, search_url)
+    if not search_content:
+        return search_url, []
+    
+    search_soup = bs(search_content, 'html.parser')
+    search_page = Search_page(url=search_url, soup_data=search_soup)
+
+    articles = search_page.get_articles()
+    logger.log(10001, search_url + ' || ' + str(len(articles)))
+    ext_path = os.path.join('./extracted_articles', search_name+".json")
+    
+    with open(ext_path, 'r+') as file:
+        try:
+            search_dict = json.load(file)
+        except json.decoder.JSONDecodeError:
+            search_dict = {}
+
+        search_dict[search_url] = list(
+            set(search_dict.get(search_url, []) + articles))
+        file.seek(0)
+        json.dump(search_dict, file)
+        file.truncate()
+    
+    return search_name, search_url, articles
+
+
+async def start_articles_parse(searchs_dict, skip_exist=True):
+    for search_name, search_article_dict in searchs_dict.items():
+        tasks = []
+        for search_url, articles in search_article_dict.items():
+            db_cnx = init_db()
+            search_page = Search_page(url=search_url)
+            search_data = get_search(search_page.db_hash(), cnx=db_cnx)
+
+            if search_data:
+                search_id = search_data.get('search_id')
+                if skip_exist and len(get_search_articles(search_url, cnx=db_cnx)) >= len(articles) - 5:
+                    db_cnx.close()
+                    continue
+
+            else:
+                search_id = insert_search(
+                    search_hash=search_page.db_hash(), **search_page.search_kwargs, cnx=db_cnx)
+
+            logger.debug('[parse_article|START] search_name: %s, search_url: %s  | articles count: %s',
+                         search_name, search_url, len(articles))
+
+            for article in articles:
+                task = asyncio.ensure_future(parse_article(
+                    article_url=article, search_url=search_url, search_id=search_id, db_cnx=db_cnx))
+                tasks.append(task)
+
+            result = await asyncio.gather(*tasks, return_exceptions=True)
+            db_cnx.close()
+
+            logger.debug('[start_parse_article|END] search_name: %s, search_url: %s  | articles count: %s',
+                         search_name, search_url, len(articles))
+
+
 async def parse_article(article_url, search_url, search_id, db_cnx, skip_exist=True):
 
-    return_data = {"search_url": search_url, "article_url": article_url, "search_id": search_id, "article_id": 0}
+    return_data = {"search_url": search_url, "article_url": article_url,
+                   "search_id": search_id, "article_id": 0}
+    
     logger.debug(log_formatter('parse_article|START', return_data))
 
     article_page = Article(url=article_url)
@@ -127,15 +153,16 @@ async def parse_article(article_url, search_url, search_id, db_cnx, skip_exist=T
             article_content = await get_soup(session, article_url)
         except Exception as e:
             return e
+    
         article_soup = bs(article_content, 'html.parser')
-        article_page._soup = article_soup    
+        article_page._soup = article_soup
 
     if not article_data:
         article_id = insert_article_data(
             **article_page.get_article_data(), cnx=db_cnx)
+    
     else:
         article_id = article_data.get('article_id')
-        
         update_article(
             **article_page.get_article_data(), cnx=db_cnx)
 
@@ -143,53 +170,19 @@ async def parse_article(article_url, search_url, search_id, db_cnx, skip_exist=T
 
     connect_search_article(search_id=search_id,
                            article_id=article_id, cnx=db_cnx)
-    logger.debug(log_formatter('parse_article|END', return_data))
     
+    logger.debug(log_formatter('parse_article|END', return_data))
     return return_data
-
-async def start_articles_parse(searchs_dict, skip_exist=True):
-    for search_name, search_article_dict in searchs_dict.items():
-        tasks = []
-        for search_url, articles in search_article_dict.items():
-            db_cnx = init_db()
-            search_page = Search_page(url=search_url)
-            search_data = get_search(search_page.db_hash(), cnx=db_cnx)
-        
-            if search_data:
-                search_id = search_data.get('search_id')
-                if skip_exist and len(get_search_articles(search_url, cnx=db_cnx)) >= len(articles) - 5 :
-                    db_cnx.close()
-                    continue
-
-            else:
-                search_id = insert_search(
-                    search_hash=search_page.db_hash(), **search_page.search_kwargs, cnx=db_cnx)
-
-            logger.debug('[parse_article|START] search_name: %s, search_url: %s  | articles count: %s',
-                        search_name, search_url, len(articles))
-
-            
-            for article in articles:
-                task = asyncio.ensure_future(parse_article(
-                    article_url=article, search_url=search_url, search_id=search_id, db_cnx=db_cnx))
-                tasks.append(task)
-
-            result = await asyncio.gather(*tasks, return_exceptions=True)
-            db_cnx.close()
-            
-            logger.debug('[start_parse_article|END] search_name: %s, search_url: %s  | articles count: %s',
-                         search_name, search_url, len(articles))
-            # logger.error('%s', result)
 
 
 def get_articles_from_dir(dir_path):
     file_names = os.listdir(dir_path)
-
     searchs_dict = {}
+    
     for file_name in file_names:
         file_path = os.path.join(dir_path, file_name)
-
         file_name = file_name.split('.')[0]
+        
         with open(file_path) as file:
             try:
                 searchs_dict[file_name] = json.load(file)
@@ -198,11 +191,13 @@ def get_articles_from_dir(dir_path):
 
     return searchs_dict
 
+
 def not_parsed_articles():
     ext_searchs_dict = get_articles_from_dir('./extracted_articles')
     # sum([len(j) for i, j in get_search_from_dir()]) # Count of all search url
     # sum([len(j) for i, j in ext_searchs_dict.items()]) # count of search with extracted article
     # len(get_all_search(cnx=db_cnx)) # count of in db searchs
+    
     missed_searchs = {}
 
     db_cnx = init_db()
@@ -211,18 +206,23 @@ def not_parsed_articles():
 
     for inp_search_name, searchs in ext_searchs_dict.items():
         missed_searchs[inp_search_name] = []
+    
         for search_url in searchs:
             search_page = Search_page(url=search_url)
+    
             if not search_page.db_hash() in hashs:
                 print(search_url)
                 missed_searchs[inp_search_name].append(search_url)
-    
+
     total_lose = sum([len(i) for i in missed_searchs.values()])
     in_file_count = sum([len(j) for i, j in ext_searchs_dict.items()])
+    
     print(f'[ {in_file_count} in input file // {len(hashs)} extracted in DB // {total_lose} lost search ]')
+    
     for search_name, searchs in missed_searchs.items():
         with open(os.path.join('./missing_searchs', search_name+'.txt'), 'a') as file:
-            file.writelines([search+'\n' for search in searchs])    
+            file.writelines([search+'\n' for search in searchs])
+
 
 def not_parsed_searchs():
     all_searchs_items = get_search_from_dir('./search_files', 'a')
@@ -232,44 +232,46 @@ def not_parsed_searchs():
     for inp_search_name, searchs in all_searchs_items:
         ext_searchs = ext_searchs_dict.get(inp_search_name)
         articles_less_searchs[inp_search_name] = []
+    
         for search in searchs:
             ext_articles = ext_searchs.get(search, [])
+    
             if len(ext_articles) <= 20:
                 articles_less_searchs[inp_search_name].append(search)
+    
         print(inp_search_name, len(articles_less_searchs[inp_search_name]))
 
     total_lose = sum([len(i) for i in articles_less_searchs.values()])
     print(total_lose)
+
     for search_name, searchs in articles_less_searchs.items():
         with open(os.path.join('./missing_searchs', search_name+'.txt'), 'a') as file:
             file.writelines([search+'\n' for search in searchs])
 
+
 if __name__ == '__main__':
-    # logging.getLogger("mainLogger").disabled = True
-    logger = logging.getLogger('fileLogger')
     debug = True
-    logger.disabled = not debug
-
+    logger = logging.getLogger('fileLogger')
+    # logging.getLogger("mainLogger").disabled = True
+    
     async_slice_size = 700
-    search_mode = 'ts'  # a: article, s: search, t: testing extracted articles :)
-    # f: free (just search accesible for every one), l: limited (just those accesible for registered), a: all
-    free_or_limited_search = 'f'
-
+    logger.disabled = not debug
+    search_mode = 'ts'  # a: article, s: search, t: testing extracted articles
+    free_or_limited_search = 'f' # f: free (just search accesible for every one), l: limited (just those accesible for registered), a: all
+    
     if search_mode == 'a':
         ext_searchs_dict = get_articles_from_dir('./extracted_articles')
         asyncio.run(start_articles_parse(ext_searchs_dict))
+
     elif search_mode == 's':
         search_items = get_search_from_dir(
             './search_files', free_or_limited_search)
         asyncio.run(start_searchs_parse(search_items))
+
     elif search_mode == 'ts':
         not_parsed_searchs()
+
     elif search_mode == 'ta':
         not_parsed_articles()
 
-#TODO: add test for knowing how many article got
-#TODO: check database for authors withc not in article_authoers
-#TODO log some data about current running task to know if is running
-
-#TODO:#TODO: { fix error of network connection in article and search scraption }
-#TODO do a better already exsit search of article skiption (for search: get count of article in db and if is near the count of article in file skip it)
+# TODO: check database for authors withc not in article_authoers
