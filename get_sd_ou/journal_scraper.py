03a2e64fab26bd7bd1359521d515f85260e452f7
Erfan <parsyab1@gmail.com>
2021-02-16 21:46:29 +0330

    add thread lock for modifing visitedds set

--- get_sd_ou/journal_scraper.py
+++ get_sd_ou/journal_scraper.py
@@ -6,7 +6,7 @@ import os
 
 from get_sd_ou.classUtil import Journal, JournalsSearch, Volume, Article
 from queue import Queue
-from threading import Thread, current_thread
+from threading import Thread, current_thread, Lock
 import sqlite3
 
 
@@ -14,7 +14,7 @@ import sqlite3
 from get_sd_ou.databaseUtil import insert_article_data, init_db
 
 
-def scrape_and_save_article(article_url_queue, db_connection):
+def scrape_and_save_article(article_url_queue, mysql_connection, sqlite_cursor):
     first_url = article_url_queue.get()
     article_url_queue.put(first_url)
 
@@ -23,11 +23,13 @@ def scrape_and_save_article(article_url_queue, db_connection):
 
         article_data, article_hash = scrape_article_url(url)
         if not article_data is None:
-            save_article_to_db(article_data, db_connection)
+            save_article_to_db(article_data, mysql_connection)
 
             article_url_queue.task_done()
-            visited.add(article_hash)
+            add_to_persistance(article_hash, sqlite_cursor)
             logger.info(f"[{current_thread().name}] - Article scraped and saved - url = {url}")
+        else:
+            logger.info(f"[{current_thread().name}] skipped article: {url}")
 
 
 def scrape_article_url(url):
@@ -57,7 +59,6 @@ def get_node_children(node):
         yield from node.iterate_volumes()
     elif isinstance(node, Volume):
         articles = node.get_articles()
-        write_visited(visited, db_cursor)
         for article in articles:
             yield article
     else:
@@ -71,25 +72,31 @@ def iterate_journal_searches():
         journal_search = journal_search.get_next_page()
 
 
-def deep_first_search_for_articles(self_node, article_url_queue):
-    if self_node.__hash__() in visited:
-        return
-    node_children = get_node_children(self_node)
+def deep_first_search_for_articles(self_node, article_url_queue, sqlite_cursor):
+    if not self_node.__hash__() in visited:
+        node_children = get_node_children(self_node)
 
-    if isinstance(self_node, Volume):  # deepest node of tree before articles is Volume
-        articles = list(node_children)
-        list(map(article_url_queue.put, articles))
-        node_children = []
-
-    for child in node_children:
-        graph[self_node] = graph.get(self_node, list()) + [child]
-        deep_first_search_for_articles(self_node=child, article_url_queue=article_url_queue)
-    visited.add(self_node.__hash__())
+        if isinstance(self_node, Volume):  # deepest node of tree before articles is Volume
+            articles = list(node_children)
+            list(map(article_url_queue.put, articles))
+        else:
+            for child in node_children:
+                graph[self_node] = graph.get(self_node, list()) + [child]
+                deep_first_search_for_articles(self_node=child, article_url_queue=article_url_queue, sqlite_cursor=sqlite_cursor)
+            add_to_persistance(self_node.__hash__(), sqlite_cursor)
+    else:
+        logger.info(f"[{current_thread().name}] skipped node: {str(self_node)}")
 
 def init_persistance(cursor=None):
     results = cursor.execute("create table if not exists visited (hash INTEGER)")
     print("persistance made")
         
+
+def add_to_persistance(item, cursor):
+    lock.acquire()
+    visited.add(int(item))
+    lock.release()
+    res = cursor.execute(f'INSERT INTO visited VALUES ({int(item)})')
 
 def write_visited(write_set, cursor=None):
     res = None
@@ -121,23 +128,27 @@ if __name__ == "__main__":
 
     conn = sqlite3.connect('example.db', check_same_thread=False)
 
-    db_cursor = conn.cursor()
-    init_persistance(db_cursor)
+    sqlite_cursor = conn.cursor()
+    init_persistance(sqlite_cursor)
 
-    file_data = load_visited(db_cursor)
+    file_data = load_visited(sqlite_cursor)
     visited = file_data if file_data else set()
     graph = {}
+    lock = Lock()
 
     article_queue = Queue(maxsize=500)
     search_thread = Thread(target=deep_first_search_for_articles,
-                           args=("ROOT", article_queue))
+                           args=("ROOT", article_queue, sqlite_cursor))
     try:
         search_thread.start()
 
         for i in range(15):
+            conn = sqlite3.connect('example.db', check_same_thread=False)
+
+            sqlite_cursor = conn.cursor()
             database_connection = init_db()
 
-            t = Thread(target=scrape_and_save_article, args=(article_queue, database_connection))
+            t = Thread(target=scrape_and_save_article, args=(article_queue, database_connection, sqlite_cursor))
             t.start()
 
         article_queue.join()
@@ -145,5 +156,6 @@ if __name__ == "__main__":
         print(e)
         print("EXCEPTION")
     finally:
-        write_visited(visited, db_cursor)
+        pass
+        # write_visited(visited, db_cursor)
         # conn.close()
