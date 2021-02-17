74d6028f263c35022cd3a4270624fdcdd9dfcac8
Erfan <parsyab1@gmail.com>
2021-02-17 08:37:22 +0330

    remove deperecated frpm app

--- get_sd_ou/app.py
+++ get_sd_ou/app.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python3
 import logging
 
-from flask import (Flask, jsonify, render_template, request)
+from flask import (Flask, jsonify)
 
 
 from get_sd_ou import databaseUtil
-from get_sd_ou.databaseUtil import (get_db_result, get_search_suggest, init_db)
+from get_sd_ou.databaseUtil import (init_db, get_status)
 
 logger = logging.getLogger('mainLogger')
 app = Flask(__name__)
@@ -22,58 +22,9 @@ def db_connection():
     return _db_connection
 
 
-@app.route('/db_search')
-def db_search():
-    return render_template('db_search.html')
-
-
-@app.route('/db_start_search', methods=['POST'])
-def db_start_search():
-    logger.debug('[app] starting task')
-    kwargs = dict(request.form)
-    kwargs['offset'] = 0
-    database_result = get_db_result(cnx=db_connection(), **kwargs)
-    
-    logger.debug('[app][db_start_search] | database_result: %s ', str(database_result)[:20])
-    
-    return render_template('db_result.html', searchs=database_result)
-
-
-@app.route('/db_suggest', methods=['POST'])
-def db_suggest():
-    logger.debug('[app][db_suggest][IN]')
-    inp = request.form.to_dict()
-    input_key = inp['key']
-    input_value = inp['value']
-    suggestion_list = get_search_suggest(input_key, input_value)
-
-    logger.debug(
-        '[app][db_suggest][OUT] | suggestion_list : %s', suggestion_list)
-    return jsonify({'suggestion_list': suggestion_list})
-
-
-@app.route('/db_suggest_all', methods=['POST'])
-def db_suggest_all():
-    logger.debug('[app][db_suggest][IN]')
-    form = dict(request.form)
-    del form['csrf_token']
-    suggests = get_search_suggest(**form)
-    res = {}
-    for key in form:
-        res[key] = []
-    for suggest in suggests:
-        for key, value in suggest.items():
-            form_value = form.get(key, False)
-            if value and key in form and value != form_value and value != ' ':
-                res[key].append(value)
-
-    logger.debug('[app][db_suggest][OUT] | res : %s', res)
-    return jsonify(res)
-
-
 @app.route('/journals/status')
-def get_status():
-    status = databaseUtil.get_status(cnx=db_connection())
+def get_db_status():
+    status = get_status(cnx=db_connection())
     return jsonify(status)
 
 
